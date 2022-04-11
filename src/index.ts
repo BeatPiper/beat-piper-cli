bf6f67586615f489c301cf178ff70d2d6f44f0f0
D3SOX <d3sox@protonmail.com>
2022-04-11 18:45:14 +0200

    feat: prettier output using chalk

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import SpotifyWebApi from 'spotify-web-api-node';
 import BeatSaverClient from './util/beatsaver.js';
 import fs from 'fs';
+import chalk from 'chalk';
 import inquirer from 'inquirer';
 import download from 'download';
 import { Track } from './types';
@@ -12,16 +13,25 @@ import { Track } from './types';
 // TODO: download progress
 // TODO: add dependabot, eslint and ci workflow
 
+console.log(chalk.underline.magenta('Welcome to Beat Piper!'));
+
 // create output folder
 if (!(await fs.existsSync('output'))) {
   await fs.mkdirSync('output');
 }
 
 // register Spotify
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_ACCESS_TOKEN } = process.env;
+if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET || !SPOTIFY_ACCESS_TOKEN) {
+  console.log(
+    chalk.red(`Please set the Spotify environment variables (See ${chalk.italic('README.md')})`)
+  );
+  process.exit(0);
+}
 const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.SPOTIFY_CLIENT_ID,
-  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-  accessToken: process.env.SPOTIFY_ACCESS_TOKEN, // TODO: auth flow
+  clientId: SPOTIFY_CLIENT_ID,
+  clientSecret: SPOTIFY_CLIENT_SECRET,
+  accessToken: SPOTIFY_ACCESS_TOKEN, // TODO: auth flow
 });
 
 // register BeatSaver
@@ -29,9 +39,8 @@ const beatSaverClient = new BeatSaverClient();
 
 // fetch user's playlists
 const me = await spotifyApi.getMe();
-
 const playlists = await spotifyApi.getUserPlaylists(me.body.id);
-console.log(`Found ${playlists.body.total} playlists`);
+console.log(chalk.green(`Found ${chalk.bold(playlists.body.total)} playlists!`));
 
 const answers = await inquirer.prompt<{
   playlist: string;
@@ -39,7 +48,7 @@ const answers = await inquirer.prompt<{
   {
     type: 'list',
     name: 'playlist',
-    message: 'Which playlist do you want to download?',
+    message: chalk.italic('Which playlist do you want to download'),
     choices: playlists.body.items.map(playlist => playlist.name),
   },
 ]);
@@ -54,7 +63,13 @@ const tracks: Track[] = playlistTracks.body.items.map(({ track }) => ({
   search: `${track.name} ${track.artists[0].name}`, // TODO: is this good as a search query?
 }));
 
-console.log(`Now searching playlist ${playlist.name} for ${tracks.length} tracks`);
+console.log(
+  chalk.blue(
+    `Now searching for maps for playlist ${chalk.bold(playlist.name)} (${chalk.bold(
+      tracks.length
+    )} tracks)…`
+  )
+);
 
 // get BeatSaver maps from tracks
 const mapResults = await beatSaverClient.searchInBatches(tracks);
@@ -64,15 +79,21 @@ const notFoundMaps = mapResults.filter(({ maps }) => maps.length === 0);
 
 // print found maps
 if (foundMaps.length) {
-  console.log(`Found ${foundMaps.map(({ track }) => track.name).join(', ')} on BeatSaver`);
+  console.log(
+    chalk.green(`Found ${chalk.bold(foundMaps.map(({ track }) => track.name).join(', '))} on BeatSaver`)
+  );
 }
 // print not found maps
 if (notFoundMaps.length) {
-  console.log(`Could not find ${notFoundMaps.map(({ track }) => track.name).join(', ')} on BeatSaver`);
+  console.log(
+    chalk.red(
+      `Could not find ${chalk.bold(notFoundMaps.map(({ track }) => track.name).join(', '))} on BeatSaver`
+    )
+  );
 }
 
 // download all maps
-console.log(`Now downloading ${foundMaps.length} maps`);
+console.log(chalk.blue(`Now downloading ${chalk.bold(foundMaps.length)} maps…`));
 await Promise.all(
   foundMaps.map(({ maps }) => download(BeatSaverClient.getLatestVersion(maps[0]).downloadURL, 'output'))
 );
